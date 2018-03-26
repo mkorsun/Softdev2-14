49dfd0d42bee4496f19f752134d719ce8bd89389
Max Korsun <mkorsun@stuy.edu>
2018-03-25 23:56:26 -0400

    ugh

--- data.js
+++ data.js
@@ -3,18 +3,18 @@ var categories=['social security, Unemployment, and labor','medicare and health'
 var data2018 = [1360,1050,517,332,491]
 var data1968 = [176,44.7,405,54.8,216]
 
-
+var data = data2018;
 var createchart = function(){
     var chart = d3.select(".chart");
     var bar = chart.selectAll("div");
     if(this.id =="2018"){
-	var barUpdate =bar.data(data2018);
+	data =data2018;
     }
     else{
-	var barUpdate = bar.data(data1968);
+	data = data1968;
     }
    
-    var barEnter = barUpdate.enter().append("div");
+    var barEnter = bar.data(data).enter().append("div");
    
     barEnter.transition().duration(3000).style("width", function(d){return d+"px";});
     
@@ -24,20 +24,20 @@ var changechart = function(e){
     var chart = d3.select(".chart");
     var bar = chart.selectAll("div");
     if(this.id =="2018"){
-	var data =data2018;
+	var newdata =data2018;
     }
     else{
-	var data = data1968;
+	var newdata = data1968;
     }
-    
-    var barEnter = bar.data(data).exit().remove().enter().append("div");
+    data = newdata;
+    var barEnter = bar.data(data).enter().append("div");
    
     barEnter.transition().duration(3000).style("width", function(d){return d+"px";});
     
 }
 
-document.getElementById("2018").addEventListener("click",createchart)
-document.getElementById("1968").addEventListener("click",createchart)
+document.getElementById("2018").addEventListener("click",changechart)
+document.getElementById("1968").addEventListener("click",changechart)
 	
 	      
 	    
