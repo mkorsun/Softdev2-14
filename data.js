af5d772b59c06d97ce58be2f51d98f7eaa572ebe
Max Korsun <mkorsun@stuy.edu>
2018-03-25 23:09:21 -0400

    test 1

---
+++ data.js
@@ -0,0 +1,32 @@
+
+var categories['social security, Unemployment, and labor','medicare and health','national defense', 'Net Interest', 'other']
+var data2018 = [1360,1050,517,332,491]
+var data1968 = [176,44.7,405,54.8,216]
+
+var chart = d3.select(".chart");
+var createchart = function(){
+    var bar = chart.selectAll("div");
+    if(this.id =="2018"){
+	var barUpdate = bar.data(data2018);
+    }
+    else{
+	var barUpdate = bar.data(data1968);
+    }
+    var barEnter = barUpdate.enter().append("div");
+    barEnter.transition().duration(function(d){return d*10;}).style("width", function(d){return d+"px";});
+    
+}
+
+document.getElementById("2018").addEventListener("click",createchart)
+document.getElementById("1968").addEventListener("click",createchart)
+	
+	      
+	    
+	
+	
+    
+	
+	
+	
+	
+	
