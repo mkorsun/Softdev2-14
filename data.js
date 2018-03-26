6b2ae16b96d820ea496efe5ec96456a3086954e2
Max Korsun <mkorsun@stuy.edu>
2018-03-26 00:00:44 -0400

    fix

--- data.js
+++ data.js
@@ -6,6 +6,8 @@ var data1968 = [176,44.7,405,54.8,216]
 var data = data2018;
 var createchart = function(){
     var chart = d3.select(".chart");
+
+    chart.selectAll("div").remove();
     var bar = chart.selectAll("div");
     if(this.id =="2018"){
 	data =data2018;
@@ -20,24 +22,8 @@ var createchart = function(){
     
 }
 
-var changechart = function(e){
-    var chart = d3.select(".chart");
-    var bar = chart.selectAll("div");
-    if(this.id =="2018"){
-	var newdata =data2018;
-    }
-    else{
-	var newdata = data1968;
-    }
-    data = newdata;
-    var barEnter = bar.data(data).enter().append("div");
-   
-    barEnter.transition().duration(3000).style("width", function(d){return d+"px";});
-    
-}
-
-document.getElementById("2018").addEventListener("click",changechart)
-document.getElementById("1968").addEventListener("click",changechart)
+document.getElementById("2018").addEventListener("click",createchart)
+document.getElementById("1968").addEventListener("click",createchart)
 	
 	      
 	    
